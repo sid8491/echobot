c180d756f677ce65deea19efea22b030679505f3
sid8491 <sid.8491@gmail.com>
2017-10-14 19:22:26 +0530

    'first'

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+import requests
+import cred
+
+app = Flask(__name__)
+
+ACCESS_TOKEN = cred.ACCESS_TOKEN
+VERIFY_TOKEN = cred.VERIFY_TOKEN
+
+def reply(user_id, msg):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"text": msg}
+    }
+    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+    print(resp.content)
+
+
+@app.route('/', methods=['GET'])
+def handle_verification():
+    if request.args['hub.verify_token'] == VERIFY_TOKEN:
+        return request.args['hub.challenge']
+    else:
+        return "Invalid verification token"
+
+
+@app.route('/', methods=['POST'])
+def handle_incoming_messages():
+    data = request.json
+    sender = data['entry'][0]['messaging'][0]['sender']['id']
+    message = data['entry'][0]['messaging'][0]['message']['text']
+    reply(sender, message)
+
+    return "ok"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
